fede01bfc0db8132208781d3b469871fd76a4b07
Jing Cao <jingcao.yale@gmail.com>
2019-02-04 16:30:12 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,9 @@
-def hello_t(names) #{|name| puts name}
-  if name.start_with("T")
-    puts name
+def hello_t(array)
+  i = 0
+
+  while i < array.length
+    yield array[i]
+    i = i + 1
   end
 end
 
